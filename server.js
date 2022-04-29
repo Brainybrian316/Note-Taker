a9d44ef4a17f9091cfa8347dcc65128645fa0a6c
Brainybrian316 <brainybrian316@gmail.com>
2022-04-29 19:28:33 -0400

    set up api use and testing api get

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 
 const app = express();
-
 const PORT= 3001;
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
+
+api.use(express.json());
+api.use(express.urlencoded({extended: true}));
+api.use(express.static('public'));
+
+
 
 app.get('/', (res, req) => {
     res.send('Hello World');
-    console.log('Hello World');
+    console.log(res.send);
+    console.log(req.send);
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${3001}`)
