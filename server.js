2e43dfb1dfae8eac42ced9dbef42383319a45661
Brainybrian316 <brainybrian316@gmail.com>
2022-04-29 22:42:04 -0400

    fixing stuff

--- server.js
+++ server.js
@@ -11,15 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-});
-
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}/`)
