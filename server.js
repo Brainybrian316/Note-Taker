397901205c1f68978bb2430706c3a3dd5faa5b50
Brainybrian316 <brainybrian316@gmail.com>
2022-04-30 13:10:20 -0400

    notes displaying correctly

--- server.js
+++ server.js
@@ -14,16 +14,18 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-app.get('/api/note', (req, res) => {
+app.get('/api/notes', (req, res) => {
     res.json(note);
 })
 
-app.post('/test/note', (req, res) => {
+app.post('/api/notes', (req, res) => {
     note.push(req.body)
     fs.writeFileSync(path.join(__dirname, './db/db.json'),
     JSON.stringify(note, null, 2))
     res.json(note);
 })
+
+  
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
